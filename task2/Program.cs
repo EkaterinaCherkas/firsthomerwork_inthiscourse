408a3f1c5fdbcef209ee5e5c8f0efd03e25e9276
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-02-22 18:21:53 +0300

    add desicion of the second task

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+if (number%2 == 0){
+Console.Write("число чётное ");
+}
+else{
+    Console.Write("число нечётное ");
+}
