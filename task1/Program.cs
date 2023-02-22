cdadfd887b209f59627181009b6e776ef8cf0b07
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-02-22 18:12:10 +0300

    add desicion of the first task

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число ");
+int number3 = int.Parse(Console.ReadLine());
+int max_num;
+max_num = number1;
+if (number2 > number1){
+    max_num = number2;
+}
+if (number3 > max_num){
+    max_num = number3;
+}
+
+Console.Write("Максимальное число " + max_num);
